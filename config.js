f0b08a26ee9c2927049aee2ea851f6a46091072d
tinkamisrael1-creator <tinkamisrael1@gmail.com>
2026-06-18 13:32:37 +0100

    Update config.js

--- config.js
+++ config.js
@@ -1,3 +1,3 @@
 module.exports = {
-  Number: "27627819241" // remplacez par votre numéro WhatsApp sans mettre le signe +
+  Number: "447447880834" // remplacez par votre numéro WhatsApp sans mettre le signe +
 };
